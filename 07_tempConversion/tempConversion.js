7de1ab7ae4847fc8fac27c058b37eee2e04c01ba
andrew-j-brown <andrewjustinbrown@protonmail.com>
2023-12-11 12:24:44 -0600

    Completed javascript-exercises, lessons 3-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let output = ((num - 32) * (5 / 9));
+  if (output % 1 != 0) {
+    output = output.toFixed(1);
+    return parseFloat(output);
+  }
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let output = ((num * (9 / 5)) + 32);
+  if (output % 1 != 0) {
+    output = output.toFixed(1);
+    return parseFloat(output);
+  }
+  return output;
 };
 
 // Do not edit below this line
